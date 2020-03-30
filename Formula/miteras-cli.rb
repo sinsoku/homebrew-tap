045ffdebce4601c5cf35fe88a61ad94d91999261
sinsoku <sinsoku.listy@gmail.com>
2020-03-31 01:48:29 +0900

    miteras-cli v0.2.0

--- Formula/miteras-cli.rb
+++ Formula/miteras-cli.rb
@@ -3,12 +3,16 @@
 class MiterasCli < Formula
   desc "A command-line tool for MITERAS."
   homepage "https://github.com/sinsoku/miteras-cli"
-  url "https://github.com/sinsoku/miteras-cli/releases/download/v0.1.1/miteras-x86_64-apple-darwin.zip"
-  sha256 "438c871ff15335fdb463795b3ee1ec27f93c043bbd1c054f00032d37386f6235"
-  version "0.1.1"
+  url "https://github.com/sinsoku/miteras-cli/releases/download/v0.2.0/miteras-x86_64-apple-darwin.zip"
+  sha256 "303572a20afe4d2f8598e1c8bea400de7017fcd76789787f22a7d0c84dd0708e"
+  version "0.2.0"
 
   def install
     bin.install "miteras"
+
+    # Install completion files
+    bash_completion.install "misc/miteras.bash" => "miteras"
+    zsh_completion.install "misc/_miteras"
   end
 
   test do
