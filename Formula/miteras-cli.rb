4414ed34888d0d9c97caa93acd2f0d5c5d372135
Takumi Shotoku <sinsoku.listy@gmail.com>
2021-07-10 16:55:32 +0900

    chore: brew style --fix

--- Formula/miteras-cli.rb
+++ Formula/miteras-cli.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class MiterasCli < Formula
-  desc "A command-line tool for MITERAS."
+  desc "Command-line tool for MITERAS"
   homepage "https://github.com/sinsoku/miteras-cli"
   url "https://github.com/sinsoku/miteras-cli/releases/download/v0.2.1/miteras-x86_64-apple-darwin.zip"
-  sha256 "c1e0ddaa8ee88251985dd4e3242021e52d335aadb80ec19ca75d397732e43657"
   version "0.2.1"
+  sha256 "c1e0ddaa8ee88251985dd4e3242021e52d335aadb80ec19ca75d397732e43657"
 
   def install
     bin.install "miteras"
@@ -16,6 +16,6 @@ class MiterasCli < Formula
   end
 
   test do
-    system "#{bin}/miteras --version"
+    system "#{bin}/miteras", "--version"
   end
 end
